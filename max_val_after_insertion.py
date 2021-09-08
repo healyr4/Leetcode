09c506a57d602a52fb582d0c49fca4622e0c1168
healyr4 <healyr4@tcd.ie>
2021-09-08 17:39:05 +0100

    Changed indentation error

--- max_val_after_insertion.py
+++ max_val_after_insertion.py
@@ -5,13 +5,13 @@ class Solution:
                 if int(n[i]) < x:
                     ret_str = n[:i] +str(x) + n[i:]
                     return ret_str
-                return n + str(x)
+            return n + str(x)
         else:
             for i in range(1,len(n)):
                 if int(n[i]) > x:
                     ret_str = n[:i] +str(x) + n[i:]
                     return ret_str
-                return n + str(x)
+            return n + str(x)
                     
             
                     
